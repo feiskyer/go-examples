59e689d83df55b8461fc9b04f9857815d000daed
Pengfei Ni <feiskyer@gmail.com>
2017-07-14 14:49:03 +0800

    update cni installation

--- networking/cnitool/install-cni.sh
+++ networking/cnitool/install-cni.sh
@@ -3,10 +3,15 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-mkdir -p $GOPATH/src/github.com/containernetworking/cni
-git clone https://github.com/containernetworking/cni.git $GOPATH/src/github.com/containernetworking/cni
-cd $GOPATH/src/github.com/containernetworking/cni
+git clone https://github.com/containernetworking/plugins $GOPATH/src/github.com/containernetworking/plugins
+cd $GOPATH/src/github.com/containernetworking/plugins
 
+# build cni plugins and copy to specified folder.
+./build.sh
+sudo mkdir -p /opt/cni/bin
+sudo cp bin/* /opt/cni/bin/
+
+# Setup network config.
 sudo mkdir -p /etc/cni/net.d
 sudo sh -c 'cat >/etc/cni/net.d/10-mynet.conf <<-EOF
 {
@@ -55,7 +60,3 @@ sudo sh -c 'cat >/etc/cni/net.d/99-loopback.conf <<-EOF
 }
 EOF'
 
-# build cni plugins and copy to specified folder
-./build.sh
-sudo mkdir -p /opt/cni/bin
-sudo cp bin/* /opt/cni/bin/
